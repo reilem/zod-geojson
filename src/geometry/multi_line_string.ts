2224352f29213cd81d2e1938e7a3fc4b6f42bfc8
Reinert Lemmens <reilemx@gmail.com>
2024-02-21 22:47:05 +0000

    Improve multi line string validation & tests

--- src/geometry/multi_line_string.ts
+++ src/geometry/multi_line_string.ts
@@ -1,12 +1,58 @@
 import { z } from "zod";
-import { GeoJSONBaseSchema, validGeometryKeys } from "./_helper";
+import {
+    bboxEquals,
+    GeoJSONBaseSchema,
+    INVALID_BBOX_ISSUE,
+    INVALID_DIMENSIONS_ISSUE,
+    INVALID_KEYS_ISSUE,
+    updateBboxForPositions,
+    validGeometryKeys,
+} from "./_helper";
 import { GeoJSONLineStringCoordinatesSchema } from "./line_string";
+
+function validMultiLineStringDimensions({ coordinates }: { coordinates: number[][][] }): boolean {
+    if (coordinates.length === 0) return true;
+    let dimension = coordinates[0][0].length;
+    return coordinates.every((ring) => ring.every((position) => position.length === dimension));
+}
+
+function validMultiLineStringBbox({ bbox, coordinates }: { bbox?: number[]; coordinates: number[][][] }): boolean {
+    if (bbox == null) {
+        return true;
+    }
+    const dimension = coordinates[0][0].length;
+    if (bbox.length !== 2 * dimension) {
+        return false;
+    }
+    const expectedBbox: number[] = [];
+    const coordinatesLen = coordinates.length;
+    for (let i = 0; i < coordinatesLen; i++) {
+        updateBboxForPositions(expectedBbox, coordinates[i]);
+    }
+    return bboxEquals(bbox, expectedBbox);
+}
 
 export const GeoJSONMultiLineStringSchema = GeoJSONBaseSchema.extend({
     type: z.literal("MultiLineString"),
     coordinates: z.array(GeoJSONLineStringCoordinatesSchema),
 })
     .passthrough()
-    .refine(validGeometryKeys);
+    .superRefine((val, ctx) => {
+        if (!validGeometryKeys(val)) {
+            ctx.addIssue(INVALID_KEYS_ISSUE);
+            return;
+        }
+        // Skip remaining checks if coordinates array is empty
+        if (!val.coordinates.length) return;
+
+        if (!validMultiLineStringDimensions(val)) {
+            ctx.addIssue(INVALID_DIMENSIONS_ISSUE);
+            return;
+        }
+        if (!validMultiLineStringBbox(val)) {
+            ctx.addIssue(INVALID_BBOX_ISSUE);
+            return;
+        }
+    });
 
 export type GeoJSONMultiLineString = z.infer<typeof GeoJSONMultiLineStringSchema>;
--- tests/geometry/multi_line_string.test.ts
+++ tests/geometry/multi_line_string.test.ts
@@ -87,10 +87,32 @@ describe("GeoJSONMultiLineString", () => {
             coordinates: [geoJsonLineString2D, geoJsonLineString3D],
         });
     });
-    it("does not allow a multi-line string with incorrect bbox", () => {
+    it("does not allow a 2D multi-line string with a non-overlapping bbox", () => {
+        // This bbox is completely outside and somewhere else from the expected bbox
         failGeoJSONMultiLineStringTest({
-            ...singleGeoJsonMultiLineString2D,
-            bbox: [30, 10, 20, 100],
+            ...multiGeoJsonMultiLineString2D,
+            bbox: [40, 40, 80, 80],
+        });
+    });
+    it("does not allow a 2D multi-line string with an intersecting bbox", () => {
+        // This bbox intersects with the expected bbox
+        failGeoJSONMultiLineStringTest({
+            ...multiGeoJsonMultiLineString2D,
+            bbox: [0.0, 1.0, 25.0, 25.0],
+        });
+    });
+    it("does not allow a 2D multi-line string with a circumscribed bbox", () => {
+        // This bbox completely encompasses the expected bbox
+        failGeoJSONMultiLineStringTest({
+            ...multiGeoJsonMultiLineString2D,
+            bbox: [0.0, 1.0, 31.0, 31.0],
+        });
+    });
+    it("does not allow a 3D multi-line string with an inscribed bbox", () => {
+        // This bbox is fully enclosed within the expected bbox
+        failGeoJSONMultiLineStringTest({
+            ...singleGeoJsonMultiLineString3D,
+            bbox: [5.0, 3.0, 1.0, 15.0, 8.0, 1.5],
         });
     });
     it("does not allow a multi-line string with invalid bbox dimensions", () => {
