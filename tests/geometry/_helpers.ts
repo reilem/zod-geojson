e6e47b85db393f6aa3e31d88b2917f2c9927a04b
Reinert Lemmens <reilemx@gmail.com>
2024-10-28 11:06:01 +0100

    Improve point testing

--- tests/geometry/_helpers.ts
+++ tests/geometry/_helpers.ts
@@ -2,14 +2,14 @@ import { expect } from "@jest/globals";
 import { ZodError, ZodSchema } from "zod";
 import { GeoJSONGeometrySchema, GeoJSONSchema } from "../../src";
 
-export function passGeoJSONGeometrySchemaTest(specificSchema: ZodSchema, value: unknown): void {
-    expect(specificSchema.parse(value)).toEqual(value);
+export function passGeoJSONGeometrySchemaTest(schemas: ZodSchema[], value: unknown): void {
+    schemas.forEach((schema) => expect(schema.parse(value)).toEqual(value));
     expect(GeoJSONGeometrySchema.parse(value)).toEqual(value);
     expect(GeoJSONSchema.parse(value)).toEqual(value);
 }
 
-export function failGeoJSONGeometrySchemaTest(specificSchema: ZodSchema, value: unknown): void {
-    expect(() => specificSchema.parse(value)).toThrow(ZodError);
+export function failGeoJSONGeometrySchemaTest(schemas: ZodSchema[], value: unknown): void {
+    schemas.forEach((schema) => expect(() => schema.parse(value)).toThrow(ZodError));
     expect(() => GeoJSONGeometrySchema.parse(value)).toThrow(ZodError);
     expect(() => GeoJSONSchema.parse(value)).toThrow(ZodError);
 }
